cd0ba28cfa7bac0a65d22e646b841c3d2e97957f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2015-02-26 13:11:49 +0100

    input, buttons e-link

---
+++ e-link/js/main.js
@@ -0,0 +1,14 @@
+var webservices = {
+  "snowball": "http://demo.nlp2rdf.org:9996/snowball",
+  "stanford": "http://demo.nlp2rdf.org:9999/stanfordcorenlpn",
+  "opennlp": "http://demo.nlp2rdf.org:9998/opennlp",
+  "spotlight": "http://demo.nlp2rdf.org:9995/spotlight"
+}
+
+function submit() {
+  console.log("Submit button pressed");
+}
+
+function reset() {
+  console.log("Reset button pressed");
+}
