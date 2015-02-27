000da244b06405aab4bbf02b8ce43d2d53214f80
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2015-02-27 14:57:50 +0100

    added table

--- e-entity/js/main.js
+++ e-entity/js/main.js
@@ -11,19 +11,53 @@ function classifier(input, cb) {
 }
 
 function submit() {
-	var input = $('#text').val();
-	cb = function(data) {
-		$("#result").val(JSON.stringify(data));
-	};
+  var input = $('#text').val();
+  cb = function(data) {
+    $("#result").val(JSON.stringify(data));
+    convertECtoTable(data);
+  };
 
-	if ($("input[name=action]:checked").val() === "ec") {
-		classifier(input, cb); 
-	} else {
-		spotlight(input, cb);
-	}
+  if ($("input[name=action]:checked").val() === "ec") {
+    classifier(input, cb);
+  } else {
+    spotlight(input, cb);
+  }
 }
 
 function reset() {
   $('#text').val("");
   $('#result').val("");
+  $('#resultTable').html(" <p>Table with results will be shown here.</p>");
+}
+
+function convertECtoTable(data) {
+  $table = $("<table></table>");
+  $header = $("<tr><th>Word</th><th>Entity</th><th>Entity URI</th><th>Type URI</th></tr>");
+  $table.append($header);
+  
+
+  for (i in data) {
+    var d = data[i];
+    var word = d.underlyingString;
+    var types = d.types;
+
+    for (j in types) {
+      $row = $("<tr></tr>");
+      $row.append("<td>" + word + "</td>");
+      var type = types[j];
+      var typeURI = type.typeURI;
+	  var entityURI = type.entityURI;
+	  var entityLabel = type.entityLabel;
+      $row.append("<td>" + entityLabel + "</td>");
+      $row.append("<td><a href=\"" + entityURI + "\">" + entityURI + "</td>");
+      $row.append("<td><a href=\"" + typeURI + "\">" + typeURI  + "</td>");
+      $table.append($row);
+	}
+
+    $table.append($row);
+  }
+
+  $('#resultTable').empty();
+  $('#resultTable').append($table);
+  console.log($table);
 }
